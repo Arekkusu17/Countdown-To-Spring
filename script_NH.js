e225ef791d8817e69024066a71853070668dd564
Alex Fernández <80935923+Arekkusu17@users.noreply.github.com>
2022-04-05 23:35:21 -0400

    Add files via upload

---
+++ script_NH.js
@@ -0,0 +1,41 @@
+const daysEl = document.getElementById('days');
+const hourEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
+
+// declaring the date of Spring Equinox
+const present = new Date();
+const presentYear = present.getFullYear();
+const springEquinox = `21 March ${presentYear}`;
+
+function countdownNorth () {
+	const springDate = new Date(springEquinox);
+	const currentDate = new Date();
+
+	if (springDate - currentDate < 0) {
+		springDate.setFullYear(presentYear + 1);
+	}
+
+	const totalSeconds = Math.floor(springDate - currentDate) / 1000;
+
+	const seconds = Math.floor(totalSeconds) % 60;
+	const minutes = Math.floor(totalSeconds / 60) % 60;
+	const hours = Math.floor(totalSeconds / 3600) % 24;
+	const days = Math.floor(totalSeconds / 86400);
+
+	daysEl.innerHTML = formatTime(days);
+	hourEl.innerHTML = formatTime(hours);
+	minsEl.innerHTML = formatTime(minutes);
+	secsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime (time) {
+	return time < 10 ? `0${time}` : time;
+}
+// we call the created function
+
+countdownNorth();
+
+// set a refreshin interval
+
+setInterval(countdownNorth, 1000);
